95dce1e4f69af779babfd8da769e98d97b6a8e1e
Prince7-jain <pjffh6547@gmail.com>
2025-06-02 17:27:32 +0530

    fix

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9nvPjh98DrbCoYwD/FiZYZZduvSphLD3EC0cYmFj8W4=\"\n}"
--- .next/static/chunks/src_app_layout_tsx_04365424._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9a168d68._.css",
-  "static/chunks/node_modules__pnpm_07a38471._.js",
-  "static/chunks/src_37d7b81e._.js"
-],
-    source: "dynamic"
-});
